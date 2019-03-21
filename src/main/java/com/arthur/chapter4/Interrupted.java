46ff79f6174383dd0ef860fb0797a5174d0f2a2e
Arthur <arthur@qq.com>
2019-03-21 20:10:43 +0800

    提交中断demo

---
+++ src/main/java/com/arthur/chapter4/Interrupted.java
@@ -0,0 +1,44 @@
+package com.arthur.chapter4;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by xusheng on 2019/3/21.
+ */
+public class Interrupted {
+
+    static class SleepRunner implements Runnable {
+
+        public static void main(String[] args) throws InterruptedException {
+            Thread sleepThread = new Thread(new SleepRunner(), "SleepThread");
+            sleepThread.setDaemon(true);
+            Thread busyThread = new Thread(new BusyRunner(), "BusyThread");
+            busyThread.setDaemon(true);
+            sleepThread.start();
+            busyThread.start();
+            TimeUnit.SECONDS.sleep(5);
+            sleepThread.interrupt();
+            busyThread.interrupt();
+            System.out.println("SleepThread interrupted is " + sleepThread.isInterrupted());
+            System.out.println("BusyThread interrupted is " + busyThread.isInterrupted());
+            SleepUtils.second(2);
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                SleepUtils.second(10);
+            }
+        }
+    }
+
+    static class BusyRunner implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+
+            }
+        }
+    }
+}
